1e731abbb4e1b39e28860ec7b7e829182d9a4319
brayanburgos@loanlogics.com <brayan.burgos@loanlogics.con>
2024-02-14 09:36:52 -0500

    Factorial challenges and fibonacci

---
+++ factorial.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        long fact = 1;
+        for (int i = 2; i<=N; i++)
+            fact = fact*i;
+        System.out.println(fact);
+    }
+}
